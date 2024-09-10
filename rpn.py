86898124e8572210192063e0d03cd464f1f69d86
Evy <110493699+EvyGithub@users.noreply.github.com>
2024-09-10 13:31:13 -0700

    eh

--- rpn.py
+++ rpn.py
@@ -1,1 +1,11 @@
+s = []
 
+def push(v) -> None:
+    global s; s += [v]
+
+def pop(v) -> float:
+    global s; return s.pop(-1)
+
+while 1:
+    userInput = input()
+    break # finish later Im too lazy D:
